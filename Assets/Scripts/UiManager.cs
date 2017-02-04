c3e4040be08dc6f2d92f0a4cdf933bab5f935ea8
lordsteggu <daniel.allison1990@gmail.com>
2017-02-04 19:04:46 +0000

    Initial Check In

---
+++ Assets/Scripts/UiManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UiManager : MonoBehaviour {
+	
+	public int healthPoints = 100;
+	public int damageDealt = 10;
+	public int level = 1;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
