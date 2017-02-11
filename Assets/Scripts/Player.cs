9d2c2d85d4f0fd2c7a7d53ae285a6981ea81c440
lordsteggu <daniel.allison1990@gmail.com>
2017-02-11 14:47:02 +0000

    Animation and movement

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,13 +4,41 @@ using UnityEngine;
 
 public class Player : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
+    public float maxSpeed = 10f;
+    bool facingRight = true;
+    Animator anim;
+
+
+
+
+    // Use this for initialization
+    void Start () {
+        anim = GetComponent<Animator>();
+
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void FixedUpdate () {
+
+        float move = Input.GetAxis("Horizontal");
+        var rigidbody = GetComponent<Rigidbody2D>();
+
+        anim.SetFloat("Speed", Mathf.Abs(move));
+
+        rigidbody.velocity = new Vector2(move * maxSpeed, rigidbody.velocity.y);
+
+        if (move > 0 && !facingRight)
+            Flip();
+        else if (move < 0 && facingRight)
+            Flip();
+            
 	}
+
+    void Flip() {
+        facingRight = !facingRight;
+
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
 }
