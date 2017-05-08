6d8c82ad9bc080f96f49403e5aa4d0095c0e2439
Tmus5 <tommussett@gmail.com>
2017-05-08 23:44:50 +0100

    Added folder for classes to be located within

---
+++ Assets/Scripts/Classes/Stats.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stats  {
+
+    public int Health { get; set; }
+    public int MaxHealth { get; set; }
+    public int Damage { get; set; }
+    public int Experience { get; set; }
+    public int AttackSpeed { get; set; }
+
+}
